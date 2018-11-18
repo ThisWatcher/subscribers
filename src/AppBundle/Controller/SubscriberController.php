92e21adbacab7fad2dfa12ebdc4c86e9c50a264c
Martynas Dapkus <martynasdapkus94@gmail.com>
2018-11-18 22:36:10 +0200

    improved form error handling

--- src/AppBundle/Controller/SubscriberController.php
+++ src/AppBundle/Controller/SubscriberController.php
@@ -144,7 +144,27 @@ class SubscriberController extends Controller
             $em->flush();
         }
 
-        throw new BadRequestException([$form->getErrors(true, true)]);
+        throw new BadRequestException($this->getErrorMessages($form));
     }
 
+
+    private function getErrorMessages(\Symfony\Component\Form\Form $form) {
+        $errors = [];
+
+        foreach ($form->getErrors() as $key => $error) {
+            if ($form->isRoot()) {
+                $errors['#'][] = $error->getMessage();
+            } else {
+                $errors[] = $error->getMessage();
+            }
+        }
+
+        foreach ($form->all() as $child) {
+            if (!$child->isValid()) {
+                $errors[$child->getName()] = $this->getErrorMessages($child);
+            }
+        }
+
+        return $errors;
+    }
 }
